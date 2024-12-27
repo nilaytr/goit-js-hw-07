4e8129245f34bf71b6d5a38d00f1852da2c245ac
Nilay <nilaytr89@gmail.com>
2024-12-28 01:10:28 +0300

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,17 +1,14 @@
-const button = document.querySelector('.change-color');
-const span = document.querySelector('span.color');
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
-function setRandomHexColor() {
-  const randomHexColor = getRandomHexColor();
-  document.querySelector('body').style.backgroundColor = `${randomHexColor}`;
+const changeColor = document.querySelector(".change-color");
+const backgroundColor = document.querySelector(".color");
 
-  span.textContent = `${randomHexColor}`;
-}
-
-button.addEventListener('click', setRandomHexColor);
+changeColor.addEventListener("click", () => {
+    const color = getRandomHexColor();
+    document.querySelector("body").style.backgroundColor = color;
+    backgroundColor.textContent = color;
+})
