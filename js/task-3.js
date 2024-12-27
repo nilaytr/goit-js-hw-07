39174f66ba73dc3381823dccda052f8fed18e40c
Nilay <nilaytr89@gmail.com>
2024-12-28 01:08:59 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,6 @@
-const input = document.querySelector('input#name-input');
-const output = document.querySelector('span#name-output');
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
 
-function replace() {
-  if (input.value) {
-    output.textContent = input.value;
-  } else {
-    output.textContent = 'Anonymous';
-  }
-}
-
-input.addEventListener('input', replace);
+nameInput.addEventListener("input", (event) => {
+    event.currentTarget.value === "" ? nameOutput.textContent = "Anonymous" : nameOutput.textContent = event.currentTarget.value;
+});
